9071d6530cad9d261ece4f2d16e72e19ebd0472e
ivipsourcecode <45613704+ivipsourcecode@users.noreply.github.com>
2021-04-06 22:54:18 +0800

    Update KeyFrame.h

--- include/KeyFrame.h
+++ include/KeyFrame.h
@@ -1,30 +1,29 @@
-/*
- *--------------------------------------------------------------------------------------------------
- * DXSLAM: A Robust and Efficient Visual SLAM System with Deep Features
- *　Author(s):
- * Dongjiang Li, Xuesong Shi, Qiwei Long, Shenghui Liu, Wei Yang, Fangshi Wang, Qi Wei, Fei Qiao qiaofei@mail.tsinghua.edu.cn
- * --------------------------------------------------------------------------------------------------
- * DXSLAM shows that deep CNN-based features can be well incorporated into modern SLAM systems, 
- * and significantly improve the system’s performance. DXSLAM is based on the famous ORB-SLAM2. 
- * If you haven't learn ORB_SLAM2 code, you'd better to be familiar with ORB_SLAM2 project first.
- *　@article{murORB2,
- *　title={{ORB-SLAM2}: an Open-Source {SLAM} System for Monocular, Stereo and {RGB-D} Cameras},
- *　author={Mur-Artal, Ra\'ul and Tard\'os, Juan D.},
- * journal={IEEE Transactions on Robotics},
- *　volume={33},
- * number={5},
- * pages={1255--1262},
- * doi = {10.1109/TRO.2017.2705103},
- * year={2017}
- *　}
- * --------------------------------------------------------------------------------------------------
- * Copyright (C) 2020, iVip Lab @ EE, THU (https://ivip-tsinghua.github.io/iViP-Homepage/). All rights reserved.
- * Licensed under the GPLv3 License;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * https://github.com/ivipsourcecode/dxslam/blob/master/License-gpl.txt
- *--------------------------------------------------------------------------------------------------
- */
+/**
+* This file is part of ORB-SLAM2.
+*
+* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/raulmur/ORB_SLAM2>
+*
+* ORB-SLAM2 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM2 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*
+* DXSLAM，A Robust and Efficient Visual SLAM System with Deep Features，is based on the famous ORB-SLAM2. 
+* Copyright (C) 2020, iVip Lab @ EE, THU (https://ivip-tsinghua.github.io/iViP-Homepage/). All rights reserved.
+* Licensed under the GPLv3 License;
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* https://github.com/ivipsourcecode/dxslam/blob/master/License-gpl.txt
+*/
 #ifndef KEYFRAME_H
 #define KEYFRAME_H
 
