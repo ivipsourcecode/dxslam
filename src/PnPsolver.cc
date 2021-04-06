b9927c073ddca86c2f0a9cb2d0179fa8dee260f8
ivipsourcecode <45613704+ivipsourcecode@users.noreply.github.com>
2021-04-06 20:58:30 +0800

    Update PnPsolver.cc

--- src/PnPsolver.cc
+++ src/PnPsolver.cc
@@ -1,3 +1,30 @@
+/*
+ *--------------------------------------------------------------------------------------------------
+ * DXSLAM: A Robust and Efficient Visual SLAM System with Deep Features
+ *　Author(s):
+ * Dongjiang Li, Xuesong Shi, Qiwei Long, Shenghui Liu, Wei Yang, Fangshi Wang, Qi Wei, Fei Qiao qiaofei@mail.tsinghua.edu.cn
+ * --------------------------------------------------------------------------------------------------
+ * DXSLAM shows that deep CNN-based features can be well incorporated into modern SLAM systems, 
+ * and significantly improve the system’s performance. DXSLAM is based on the famous ORB-SLAM2. 
+ * If you haven't learn ORB_SLAM2 code, you'd better to be familiar with ORB_SLAM2 project first.
+ *　@article{murORB2,
+ *　title={{ORB-SLAM2}: an Open-Source {SLAM} System for Monocular, Stereo and {RGB-D} Cameras},
+ *　author={Mur-Artal, Ra\'ul and Tard\'os, Juan D.},
+ * journal={IEEE Transactions on Robotics},
+ *　volume={33},
+ * number={5},
+ * pages={1255--1262},
+ * doi = {10.1109/TRO.2017.2705103},
+ * year={2017}
+ *　}
+ * --------------------------------------------------------------------------------------------------
+ * Copyright (C) 2020, iVip Lab @ EE, THU (https://ivip-tsinghua.github.io/iViP-Homepage/). All rights reserved.
+ * Licensed under the GPLv3 License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://github.com/ivipsourcecode/dxslam/blob/master/License-gpl.txt
+ *--------------------------------------------------------------------------------------------------
+ */
 #include <iostream>
 
 #include "PnPsolver.h"
